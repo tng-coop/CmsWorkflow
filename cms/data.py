0a084d7aacd5bf887868be624fb9892670959895
Yasuaki Kudo <yasu@yasuaki.com>
2025-06-08 22:16:04 +0900

    Add content seeder and test

--- cms/data.py
+++ cms/data.py
@@ -1,5 +1,11 @@
 from .types import ContentType
-from .models import HTMLContent, Revision
+from .models import (
+    HTMLContent,
+    PDFContent,
+    OfficeAddressContent,
+    EventScheduleContent,
+    Revision,
+)
 
 
 def seed_users():
@@ -28,3 +34,34 @@ def sample_content(users):
         revisions=[revision],
         categories=[],
     )
+
+
+def seed_example_contents(users):
+    """Return example content objects for each supported type."""
+    timestamp = "2025-06-08T12:00:00"
+    contents = []
+    for ct in ContentType:
+        for i in range(2):
+            rev = Revision(
+                uuid=f"{ct.value}-{i}-rev",
+                last_updated=timestamp,
+                attributes={"title": f"Example {ct.value} {i}"},
+            )
+            base_kwargs = dict(
+                uuid=f"{ct.value}-{i}",
+                title=f"Example {ct.value} {i}",
+                created_by=users["editor"]["uuid"],
+                created_at=timestamp,
+                timestamps=timestamp,
+                revisions=[rev],
+                categories=[],
+            )
+            if ct is ContentType.HTML:
+                contents.append(HTMLContent(**base_kwargs))
+            elif ct is ContentType.PDF:
+                contents.append(PDFContent(**base_kwargs))
+            elif ct is ContentType.OFFICE_ADDRESS:
+                contents.append(OfficeAddressContent(**base_kwargs))
+            elif ct is ContentType.EVENT_SCHEDULE:
+                contents.append(EventScheduleContent(**base_kwargs))
+    return contents
